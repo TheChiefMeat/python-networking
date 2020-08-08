405ef8ea714ce260d7fc4ae348e362f75b0cb543
TheChiefMeat <TheChiefMeat@gmail.com>
2020-08-08 17:02:09 +0100

    Defined msg variable

--- mailing-client/main.py
+++ mailing-client/main.py
@@ -2,7 +2,7 @@ import smtplib
 from email import encoders
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from email.mem.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 
@@ -12,3 +12,8 @@ with open('password.txt', 'r') as f:
     password f.read()
 
 server.login('test@gmail.com' password)
+
+msg = MIMEMultipart()
+msg['From'] = 'MyName'
+msg['To'] = 'ToPerson@gmail.com'
+msg['Subject'] = 'Just A Test'
