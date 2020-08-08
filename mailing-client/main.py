3c161dd17b9ff39a633a178c80b84b4c2864688b
TheChiefMeat <TheChiefMeat@gmail.com>
2020-08-08 16:59:41 +0100

    Added imports

--- mailing-client/main.py
+++ mailing-client/main.py
@@ -1,4 +1,8 @@
 import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mem.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 
